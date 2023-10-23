1960047e17d75f525c9181dac50d01d6f1d7d7ec
arash bahariye <bahariye.arash@gmail.com>
2023-10-23 19:35:01 +0200

    add landing page

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import Wrapper from "../assets/wrappers/LandingPage";
+import main from "../assets/images/main.svg";
+import { Link } from "react-router-dom";
+import { Logo } from "../components";
 
 const Landing = () => {
   return (
-    <div>
-      <h1>Landing</h1>
-    </div>
+    <Wrapper>
+      <nav>
+        <Logo></Logo>
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Est mollit ea ea dolore anim. Fugiat ut tempor dolore aute aliqua
+            eiusmod sunt. Enim officia aliquip anim commodo.
+          </p>
+          <Link to="/register" classNamebtn register-link>
+            Register
+          </Link>
+          <Link to="/login" classNamebtn register-link>
+            Login
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-image"></img>
+      </div>
+    </Wrapper>
   );
 };
 
