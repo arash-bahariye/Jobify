fad0fba5b68a7293136f84da02f4ad98ef221046
arash bahariye <bahariye.arash@gmail.com>
2023-10-24 16:16:37 +0200

    add jobs router

---
+++ routes/jobsRouter.js
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import {
+  validateJobInput,
+  validateIdParam,
+} from "../middleware/validationMiddleware.js";
+
+const router = Router();
+
+import {
+  getAllJobs,
+  getJob,
+  createJob,
+  updateJob,
+  deleteJob,
+} from "../controllers/jobController.js";
+
+router.route("/").get(getAllJobs).post(validateJobInput, createJob);
+router
+  .route("/:id")
+  .get(validateIdParam, getJob)
+  .patch(validateIdParam, validateJobInput, updateJob)
+  .delete(validateIdParam, deleteJob);
+
+export default router;
