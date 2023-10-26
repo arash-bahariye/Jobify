4e4649e951eadf08c4f7c2e8aa4126ac06f5e64b
arash bahariye <bahariye.arash@gmail.com>
2023-10-26 16:46:59 +0200

    import actions and loaders

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import {
   HomeLayout,
@@ -7,7 +6,22 @@ import {
   Login,
   DashboardLayout,
   Error,
+  AddJob,
+  Stats,
+  AllJobs,
+  Profile,
+  Admin,
+  EditJob,
 } from "./pages";
+import { action as registerAction } from "./pages/Register";
+import { action as loginAction } from "./pages/Login";
+import { action as addJob } from "./pages/AddJob";
+import { action as editJobAction } from "./pages/EditJob";
+import { loader as adminLoader } from "./pages/Admin";
+import { loader as editJobLoader } from "./pages/EditJob";
+import { loader as dashboardLoader } from "./pages/DashboardLayout";
+import { loader as allJobsLoader } from "./pages/AllJobs";
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -21,14 +35,48 @@ const router = createBrowserRouter([
       {
         path: "register",
         element: <Register />,
+        action: registerAction,
       },
       {
         path: "login",
         element: <Login />,
+        action: loginAction,
       },
       {
         path: "dashboard",
         element: <DashboardLayout />,
+        loader: dashboardLoader,
+        children: [
+          {
+            index: true,
+            element: <AddJob />,
+            action: addJob,
+          },
+          {
+            path: "all-jobs",
+            element: <AllJobs />,
+            loader: allJobsLoader,
+          },
+          {
+            path: "edit-job/:id",
+            element: <EditJob />,
+            loader: editJobLoader,
+            action: editJobAction,
+          },
+          {
+            path: "stats",
+            element: <Stats />,
+          },
+          {
+            path: "profile",
+            element: <Profile />,
+          },
+          {
+            path: "admin",
+            element: <Admin />,
+            loader: adminLoader,
+          },
+        ],
       },
     ],
   },
